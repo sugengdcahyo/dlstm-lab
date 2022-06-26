35fdf8d98e1bacf503cbfa998ab895a7da59c6d7
Mekarsa Base <mekarsa@sugengdcahyo.com>
2022-06-26 23:59:30 +0700

    setup index of response predict point from 1

--- src/services/viewsets/predict.py
+++ src/services/viewsets/predict.py
@@ -14,7 +14,7 @@ class PredictViewsets(generics.CreateAPIView, viewsets.GenericViewSet):
 
         predicted = []
         for i in range(int(request.data['range'])):
-            predicted.append({"time": i, "value": random.randint(800, 3000)})
+            predicted.append({"time": i+1, "value": random.randint(800, 3000)})
 
         return Response(predicted, status=status.HTTP_200_OK)
 
