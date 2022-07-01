bf50ca519229cbf7febb1f3c8a1bf5f0c0a6082d
sugengdcahyo <sugengdcahyo@gmail.com>
2022-07-02 02:48:36 +0700

    update endpoint

--- src/services/routes.py
+++ src/services/routes.py
@@ -2,7 +2,7 @@ from rest_framework import routers
 
 from services.viewsets.rates import RatesViewsets
 from services.viewsets.currency import CurrencyViewsets
-from services.viewsets.dashboard import DashboardViewsets, DashboardTableViewsets
+from services.viewsets.dashboard import DashboardViewsets, DashboardHistogramViewsets
 from services.viewsets.histories import HistoriesViewsets, HistoryOneDayViewsets
 from .viewsets.models import ModelViewsets
 from .viewsets.predict import PredictViewsets
@@ -11,7 +11,7 @@ router = routers.DefaultRouter()
 
 router.register('currency', CurrencyViewsets, basename='currency-histories')
 router.register('dashboard/charts', DashboardViewsets, basename='dashboard-charts-viewsets')
-router.register('dashboard/tables', DashboardTableViewsets, basename='dashboard-tables-viewsets')
+router.register('dashboard/histogram', DashboardHistogramViewsets, basename='dashboard-histogram-viewsets')
 router.register('histories', HistoriesViewsets, basename='histories-viewsets')
 router.register('latest-day', HistoryOneDayViewsets, basename='latest-day-histories-viewsets')
 router.register('predict', PredictViewsets, basename='predict-viewsets')
